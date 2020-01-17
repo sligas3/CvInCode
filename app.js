15f446bcef708dc5f053102aca41e52343f5afb1
Silvio <sligas3@gmail.com>
2020-01-16 22:31:45 -0300

    añadiendo nodemailer

--- app.js
+++ app.js
@@ -1,11 +1,42 @@
 var express = require('express'),
 	app = express(),
-	bodyParser = require('body-parser');
+	bodyParser = require('body-parser'),
+	nodemailer = require('nodemailer');
 
 // APP CONFIG
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: true }));
+
+// NODEMAILER CONFIG
+app.post('/contacto', function(req, res) {
+	// Definimos el transporter
+	var transporter = nodemailer.createTransport({
+		service: 'Gmail',
+		auth: {
+			user: 'sligas3arg@gmail.com',
+			pass: 'silvio_20'
+		}
+	});
+	// Definimos el email
+	var mailOptions = {
+		from: req.body.nombre,
+		to: req.body.email,
+		subject: req.body.asunto,
+		text: req.body.mensaje
+	};
+	// Enviamos el email
+	transporter.sendMail(mailOptions, function(error, info) {
+		if (error) {
+			console.log(error);
+			res.send(500, err.message);
+		} else {
+			// console.log('Email sent');
+			// res.status(200).jsonp(req.body);
+			res.redirect('/');
+		}
+	});
+});
 
 // RUTAS
 app.get('/', function(req, res) {
