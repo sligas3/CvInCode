2e02805c4ccff5a69c370d4a5320cb19c2e9e785
Silvio <sligas3@gmail.com>
2020-01-17 10:58:58 -0300

    modificando algunos estilos del perfil y header (navbar, icons)

--- app.js
+++ app.js
@@ -8,36 +8,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-// NODEMAILER CONFIG
-app.post('/contacto', function(req, res) {
-	// Definimos el transporter
-	var transporter = nodemailer.createTransport({
-		service: 'Gmail',
-		auth: {
-			user: 'sligas3arg@gmail.com',
-			pass: 'silvio_20'
-		}
-	});
-	// Definimos el email
-	var mailOptions = {
-		from: req.body.nombre,
-		to: req.body.email,
-		subject: req.body.asunto,
-		text: req.body.mensaje
-	};
-	// Enviamos el email
-	transporter.sendMail(mailOptions, function(error, info) {
-		if (error) {
-			console.log(error);
-			res.send(500, err.message);
-		} else {
-			// console.log('Email sent');
-			// res.status(200).jsonp(req.body);
-			res.redirect('/');
-		}
-	});
-});
-
 // RUTAS
 app.get('/', function(req, res) {
 	res.render('perfil');
@@ -45,6 +15,31 @@ app.get('/', function(req, res) {
 
 app.get('/contacto', function(req, res) {
 	res.render('contacto');
+});
+
+// NODEMAILER CONFIG
+app.post('/contacto', function(req, res) {
+	var transporter = nodemailer.createTransport({
+		service: 'Gmail',
+		auth: {
+			user: 'sligas3arg@gmail.com',
+			pass: 'silvio_20'
+		}
+	});
+	var mailOptions = {
+		from: req.body.nombre,
+		to: req.body.email,
+		subject: req.body.asunto,
+		text: req.body.mensaje
+	};
+	transporter.sendMail(mailOptions, function(error, info) {
+		if (error) {
+			console.log(error);
+			res.send(500, err.message);
+		} else {
+			res.redirect('/');
+		}
+	});
 });
 
 // SERVIDOR
